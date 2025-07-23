ec52a082b7d7bbeed1ff24dcffe33dbe7a86247b
naveenchelo <chelonaveen07@gmail.com>
2025-07-23 21:18:54 +0530

    fix: shared styles link to remote modules

--- projects/e-waste-admin/src/app/app-routing.module.ts
+++ projects/e-waste-admin/src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'requests',
+    loadChildren: () =>
+      import('./features/requests/requests.module').then(
+        (m) => m.RequestsModule
+      ),
+  },
+  {
+    path: 'cities',
+    loadChildren: () =>
+      import('./features/cities/cities.module').then((m) => m.CitiesModule),
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./features/users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'reports',
+    loadChildren: () =>
+      import('./features/reports/reports.module').then((m) => m.ReportsModule),
+  },
+  {
+    path: 'settings',
+    loadChildren: () =>
+      import('./features/settings/settings.module').then(
+        (m) => m.SettingsModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- projects/e-waste-shell/src/app/app.module.ts
+++ projects/e-waste-shell/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedStylesModule } from '../../../shared-styles/src/public-api';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedStylesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
